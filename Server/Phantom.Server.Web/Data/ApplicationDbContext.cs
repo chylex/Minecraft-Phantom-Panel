cbaa2a4a72d027331dce6b6fdf56ccb094c056f5
chylex <contact@chylex.com>
2022-07-22 12:57:40 +0200

    WIP

---
+++ Server/Phantom.Server.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Phantom.Server.Web.Data;
+
+public class ApplicationDbContext : IdentityDbContext {
+	public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+		: base(options) {}
+}
