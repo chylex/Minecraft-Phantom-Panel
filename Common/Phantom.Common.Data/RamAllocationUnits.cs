749b436d01bbfa5c1b5420847ef683bf270ffce6
chylex <contact@chylex.com>
2022-08-15 04:27:34 +0200

    WIP

--- Common/Phantom.Common.Data/RamAllocationUnits.cs
+++ Common/Phantom.Common.Data/RamAllocationUnits.cs
@@ -8,14 +8,50 @@ namespace Phantom.Common.Data;
 [MessagePackObject]
 public readonly record struct RamAllocationUnits {
 	private const int ConversionFactorMegabytes = 256;
+	
+	private const ushort MaximumUnits = ushort.MaxValue;
+	private const int MaximumMegabytes = (MaximumUnits + 1) * ConversionFactorMegabytes;
 
-	public static RamAllocationUnits FromMegabytesFloored(int megabytes) {
-		if (megabytes < ConversionFactorMegabytes) {
-			throw new ArgumentOutOfRangeException(nameof(megabytes), "Must be at least 256 MB.");
+	/// <summary>
+	/// Converts an amount of <paramref name="megabytes"/> to <see cref="RamAllocationUnits"/>.
+	/// </summary>
+	/// <exception cref="ArgumentOutOfRangeException">If the value of <paramref name="megabytes"/> is not positive, not a multiple of <see cref="ConversionFactorMegabytes"/>, or exceeds the hard limit.</exception>
+	public static RamAllocationUnits FromMegabytes(int megabytes) {
+		if (megabytes % ConversionFactorMegabytes != 0) {
+			throw new ArgumentOutOfRangeException(nameof(megabytes), "Must be a multiple of " + ConversionFactorMegabytes + " MB.");
 		}
 
-		int units = (megabytes - ConversionFactorMegabytes) / ConversionFactorMegabytes;
-		return new RamAllocationUnits((ushort) Math.Clamp(units, ushort.MinValue, ushort.MaxValue));
+		long units = ((long) megabytes - ConversionFactorMegabytes) / ConversionFactorMegabytes;
+		if (units < 0) {
+			throw new ArgumentOutOfRangeException(nameof(megabytes), "Must be at least " + ConversionFactorMegabytes + " MB.");
+		}
+		else if (units > MaximumUnits) {
+			throw new ArgumentOutOfRangeException(nameof(megabytes), "Must be at most " + MaximumMegabytes + " MB.");
+		}
+		
+		return new RamAllocationUnits((ushort) Math.Clamp(units, 0, MaximumUnits));
+	}
+
+	/// <summary>
+	/// Converts a string in the format &lt;number&gt;{M|G} (case-insensitive) to <see cref="RamAllocationUnits"/>.
+	/// </summary>
+	/// <exception cref="ArgumentOutOfRangeException">If the <paramref name="definition"/> is in the incorrect format, or the value cannot be converted via <see cref="FromMegabytes"/>.</exception>
+	public static RamAllocationUnits FromString(ReadOnlySpan<char> definition) {
+		if (definition.IsEmpty) {
+			throw new ArgumentOutOfRangeException(nameof(definition), "Must not be empty.");
+		}
+
+		int unitMultiplier = char.ToUpperInvariant(definition[^1]) switch {
+			'M' => 1,
+			'G' => 1024,
+			_ => throw new ArgumentOutOfRangeException(nameof(definition), "Must end with 'M' or 'G'.")
+		};
+
+		if (!int.TryParse(definition[..^1], out int size)) {
+			throw new ArgumentOutOfRangeException(nameof(definition), "Must begin with a number.");
+		}
+		
+		return FromMegabytes(size * unitMultiplier);
 	}
 	
 	[Key(0)]
---
+++ Tests.Phantom.Common.Data/RamAllocationUnitsTests.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using Phantom.Common.Data;
+
+namespace Tests.Phantom.Common.Data;
+
+[TestFixture]
+public sealed class RamAllocationUnitsTests {
+	public sealed class FromMegabytes {
+		private Action CallFromMegabytes(int value) {
+			return () => RamAllocationUnits.FromMegabytes(value);
+		}
+
+		[TestCase(1)]
+		[TestCase(-1)]
+		[TestCase(255)]
+		[TestCase(-255)]
+		[TestCase(int.MaxValue)]
+		[TestCase(int.MinValue + 1)]
+		public void NotMultipleOf256MegabytesThrows(int value) {
+			Assert.That(CallFromMegabytes(value), Throws.Exception.TypeOf<ArgumentOutOfRangeException>().With.Message.StartsWith("Must be a multiple of 256 MB."));
+		}
+		
+		[TestCase(0)]
+		[TestCase(-256)]
+		[TestCase(int.MinValue)]
+		public void LessThan256MegabytesThrows(int value) {
+			Assert.That(CallFromMegabytes(value), Throws.Exception.TypeOf<ArgumentOutOfRangeException>().With.Message.StartsWith("Must be at least 256 MB."));
+		}
+
+		[TestCase(16777216 + 256)]
+		[TestCase(int.MaxValue - 255)]
+		public void MoreThan16TerabytesThrows(int value) {
+			Assert.That(CallFromMegabytes(value), Throws.Exception.TypeOf<ArgumentOutOfRangeException>().With.Message.StartsWith("Must be at most " + (256 * 65536) + " MB."));
+		}
+
+		[TestCase(256)]
+		[TestCase(512)]
+		[TestCase(1024)]
+		[TestCase(65536)]
+		[TestCase(16777216)]
+		public void ValidValueReturnsSameValueInMegabytes(int value) {
+			Assert.That(RamAllocationUnits.FromMegabytes(value).InMegabytes, Is.EqualTo(value));
+		}
+	}
+}
