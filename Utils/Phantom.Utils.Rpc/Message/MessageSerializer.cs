dd89fb6150e417f3eaf9ceacdb83176ddcbbbaa6
chylex <contact@chylex.com>
2022-09-16 16:00:34 +0200

    WIP

--- Utils/Phantom.Utils.Rpc/Message/MessageSerializer.cs
+++ Utils/Phantom.Utils.Rpc/Message/MessageSerializer.cs
@@ -1,4 +1,5 @@
-﻿using MessagePack;
+﻿using System.Buffers.Binary;
+using MessagePack;
 using MessagePack.Resolvers;
 
 namespace Phantom.Utils.Rpc.Message;
@@ -20,26 +21,14 @@ static class MessageSerializer {
 	}
 
 	public static void WriteCode(Stream stream, ushort value) {
-		stream.WriteByte((byte) (value & 0xFF));
-		stream.WriteByte((byte) ((value >> 8) & 0xFF));
-	}
-
-	public static void WriteSequenceId(Stream stream, uint value) {
-		stream.WriteByte((byte) (value & 0xFF));
-		stream.WriteByte((byte) ((value >> 8) & 0xFF));
-		stream.WriteByte((byte) ((value >> 16) & 0xFF));
-		stream.WriteByte((byte) ((value >> 24) & 0xFF));
+		Span<byte> buffer = stackalloc byte[2];
+		BinaryPrimitives.WriteUInt16LittleEndian(buffer, value);
+		stream.Write(buffer);
 	}
 
 	public static ushort ReadCode(ref ReadOnlyMemory<byte> memory) {
-		ushort value = (ushort) (memory.Span[0] | (memory.Span[1] << 8));
+		ushort value = BinaryPrimitives.ReadUInt16LittleEndian(memory.Span);
 		memory = memory[2..];
-		return value;
-	}
-
-	public static uint ReadSequenceId(ref ReadOnlyMemory<byte> memory) {
-		uint value = (uint) (memory.Span[0] | (memory.Span[1] << 8) | (memory.Span[2] << 16) | (memory.Span[3] << 24));
-		memory = memory[4..];
 		return value;
 	}
 }
