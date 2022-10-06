73a5eb5422c97d3b4af6c63af2916ac118c4ccc3
chylex <contact@chylex.com>
2022-10-06 03:35:03 +0200

    Add logging with Serilog

---
+++ Common/Phantom.Common.Logging/LoggerExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Serilog;
+
+namespace Phantom.Common.Logging; 
+
+public static class LoggerExtensions {
+	private static readonly string HeadingPadding = new (' ', 23);
+	private static readonly string HeadingLine = new ('-', Math.Min(50, Console.BufferWidth));
+	
+	private static readonly string Heading1 = HeadingLine + '\n' + HeadingPadding;
+	private static readonly string Heading2 = '\n' + HeadingPadding + HeadingLine;
+
+	public static void InformationHeading(this ILogger logger, string title) {
+		logger.Information("{Heading1}{Title}{Heading2}", Heading1, title, Heading2);
+	}
+}
---
+++ Common/Phantom.Common.Logging/PhantomLogger.cs
@@ -0,0 +1,56 @@
+﻿using System.Globalization;
+using Serilog;
+using Serilog.Core;
+using Serilog.Events;
+using Serilog.Sinks.SystemConsole.Themes;
+
+namespace Phantom.Common.Logging;
+
+public static class PhantomLogger {
+	public static Logger Root { get; } = CreateBaseLogger("[{Timestamp:HH:mm:ss} {Level:u}] {Message:lj}{NewLine}{Exception}");
+	private static Logger Base { get; } = CreateBaseLogger("[{Timestamp:HH:mm:ss} {Level:u}] [{Category}] {Message:lj}{NewLine}{Exception}");
+
+	private static LogEventLevel GetDefaultLevel() {
+		#if DEBUG
+		return LogEventLevel.Verbose;
+		#else
+		return LogEventLevel.Information;
+		#endif
+	}
+
+	private static Logger CreateBaseLogger(string template) =>
+		new LoggerConfiguration()
+			.MinimumLevel.Is(GetDefaultLevel())
+			.MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+			.MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Warning)
+			.MinimumLevel.Override("Microsoft.EntityFrameworkCore.Database.Command", LogEventLevel.Warning)
+			.Filter.ByExcluding(static e => e.Exception is OperationCanceledException)
+			.Enrich.FromLogContext()
+			.WriteTo.Console(outputTemplate: template, formatProvider: CultureInfo.InvariantCulture, theme: AnsiConsoleTheme.Literate)
+			.CreateLogger();
+
+	public static ILogger Create(string name) {
+		return Base.ForContext("Category", name);
+	}
+
+	public static ILogger Create(Type type) {
+		return Create(type.Name);
+	}
+
+	public static ILogger Create<T>() {
+		return Create(typeof(T).Name);
+	}
+
+	public static ILogger Create<T>(string name) {
+		return Create(typeof(T).Name + ":" + name);
+	}
+
+	public static ILogger Create<T1, T2>() {
+		return Create(typeof(T1).Name + ":" + typeof(T2).Name);
+	}
+
+	public static void Dispose() {
+		Root.Dispose();
+		Base.Dispose();
+	}
+}
