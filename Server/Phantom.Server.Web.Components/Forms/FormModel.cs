6ded2575cb09d7193954869f2075d31852c2c0b4
chylex <contact@chylex.com>
2022-10-08 12:13:29 +0200

    Refactor Blazor form context

---
+++ Server/Phantom.Server.Web.Components/Forms/FormModel.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+using Phantom.Server.Web.Components.Utils;
+
+namespace Phantom.Server.Web.Components.Forms; 
+
+public abstract class FormModel {
+	public EditContext EditContext { get; }
+	public FormButtonSubmit.SubmitModel SubmitModel { get; } = new ();
+
+	protected FormModel() {
+		EditContext = BootstrapEditContext.Create(this);
+	}
+	
+	protected FormModel(EditContext editContext) {
+		EditContext = editContext;
+	}
+}
