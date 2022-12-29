6472134f9a9b1c1433a4f1a966b94625489bf8c8
chylex <contact@chylex.com>
2022-12-29 18:20:24 +0100

    Update all NuGet packages

--- Utils/Phantom.Utils.Rpc/Message/MessageSerializer.cs
+++ Utils/Phantom.Utils.Rpc/Message/MessageSerializer.cs
@@ -5,7 +5,7 @@ using MemoryPack;
 namespace Phantom.Utils.Rpc.Message;
 
 static class MessageSerializer {
-	private static readonly MemoryPackSerializeOptions SerializerOptions = MemoryPackSerializeOptions.Utf8;
+	private static readonly MemoryPackSerializerOptions SerializerOptions = MemoryPackSerializerOptions.Utf8;
 
 	public static void Serialize<TMessage, TListener>(IBufferWriter<byte> destination, TMessage message) where TMessage : IMessage<TListener> {
 		MemoryPackSerializer.Serialize(typeof(TMessage), destination, message, SerializerOptions);
