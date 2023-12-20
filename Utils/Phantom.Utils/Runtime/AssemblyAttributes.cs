1ded0e50b2cd527550ae79b21b4cfcaf513193bb
chylex <contact@chylex.com>
2023-12-20 04:26:48 +0100

    Redesign Web navigation

--- Utils/Phantom.Utils/Runtime/AssemblyAttributes.cs
+++ Utils/Phantom.Utils/Runtime/AssemblyAttributes.cs
@@ -4,6 +4,6 @@ namespace Phantom.Utils.Runtime;
 
 public static class AssemblyAttributes {
 	public static string GetFullVersion(Assembly assembly) {
-		return assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion.Replace('+', '/') ?? string.Empty;
+		return assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion.Replace('+', '-') ?? string.Empty;
 	}
 }
