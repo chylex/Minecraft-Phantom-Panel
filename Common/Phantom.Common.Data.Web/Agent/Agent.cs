ccf3ecb180688321332e203cb1e31d024c22692e
chylex <contact@chylex.com>
2025-12-31 17:29:36 +0100

    WIP

--- Common/Phantom.Common.Data.Web/Agent/Agent.cs
+++ Common/Phantom.Common.Data.Web/Agent/Agent.cs
@@ -15,4 +15,32 @@ public sealed partial record Agent(
 ) {
 	[MemoryPackIgnore]
 	public RamAllocationUnits? AvailableMemory => RuntimeInfo.MaxMemory - Stats?.RunningInstanceMemory;
+	
+	public Agent With(Update update) => new (
+		update.AgentGuid.Or(AgentGuid),
+		update.Configuration.Or(Configuration),
+		update.ConnectionKey.Or(ConnectionKey),
+		update.RuntimeInfo.Or(RuntimeInfo),
+		update.Stats.Or(Stats),
+		update.ConnectionStatus.Or(ConnectionStatus)
+	);
+	
+	[MemoryPackable(GenerateType.VersionTolerant)]
+	public sealed partial record Update(
+		[property: MemoryPackOrder(0)] Optional<Guid> AgentGuid,
+		[property: MemoryPackOrder(1)] Optional<AgentConfiguration> Configuration,
+		[property: MemoryPackOrder(2)] Optional<ImmutableArray<byte>> ConnectionKey,
+		[property: MemoryPackOrder(3)] Optional<AgentRuntimeInfo> RuntimeInfo,
+		[property: MemoryPackOrder(4)] OptionalNullable<AgentStats> Stats,
+		[property: MemoryPackOrder(5)] Optional<IAgentConnectionStatus> ConnectionStatus
+	) {
+		public Update Merge(Update newer) => new (
+			newer.AgentGuid.Or(AgentGuid),
+			newer.Configuration.Or(Configuration),
+			newer.ConnectionKey.Or(ConnectionKey),
+			newer.RuntimeInfo.Or(RuntimeInfo),
+			newer.Stats.Or(Stats),
+			newer.ConnectionStatus.Or(ConnectionStatus)
+		);
+	}
 }
---
+++ Common/Phantom.Common.Data/OptionalNullable.cs
@@ -0,0 +1,42 @@
+﻿using MemoryPack;
+
+namespace Phantom.Common.Data;
+
+[MemoryPackable]
+public readonly partial struct OptionalNullable<T> {
+	[MemoryPackOrder(0)]
+	public bool HasValue { get; }
+	
+	[MemoryPackOrder(1)]
+	public T? Value {
+		get {
+			if (HasValue) {
+				return field;
+			}
+			else {
+				throw new InvalidOperationException();
+			}
+		}
+	}
+	
+	public OptionalNullable() : this(hasValue: false, value: default) {}
+	public OptionalNullable(T? value) : this(hasValue: true, value) {}
+	
+	[MemoryPackConstructor]
+	private OptionalNullable(bool hasValue, T? value) {
+		this.HasValue = hasValue;
+		this.Value = value;
+	}
+	
+	public T? Or(T? fallbackValue) {
+		return HasValue ? Value : fallbackValue;
+	}
+	
+	public OptionalNullable<T> Or(OptionalNullable<T> fallbackOptional) {
+		return HasValue ? Value : fallbackOptional;
+	}
+	
+	public static implicit operator OptionalNullable<T>(T? value) {
+		return new OptionalNullable<T>(value);
+	}
+}
